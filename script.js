d00e8154c2dab497795c622dde13f62c846a1bd3
Lucas Beauloi <l.beauloi@gmail.com>
2023-10-06 16:22:28 +0200

    fin de journée

--- script.js
+++ script.js
@@ -4,22 +4,38 @@
 // créer le bouton en js
 // je sais pas encore comment mais "intégrer" le fichier json au js pour pouvoir prendre les éléments que l'ont veut mettre dans la liste
 // eventlistener sur le bouton pour créer une ul
-
+let title = document.createElement("h1");
 let bouton = document.createElement("button");
 bouton.textContent = "click";
+title.textContent = "Asynchrone exercice 1";
+document.body.appendChild(title);
 document.body.appendChild(bouton);
 
+
 bouton.addEventListener("click", function createList() {
-  let newList = document.createElement("ul");
-  newList.textContent = "han ouais";
-  document.body.appendChild(newList);
+  fetch("superheros.json")
+    .then((response) => response.json())
+    .then((data) => {
+      // Accédez à la propriété "members" qui contient les membres de l'équipe
+      let members = data.members;
 
-  fetch("https://mdn.github.io/learning-area/javascript/oojs/json/superheroes.json")
-  .then(response => {
-    // Gérer la réponse ici
-  })
-  .catch(error => {
-    // Gérer les erreurs ici
-  });
+      // Créez la liste <ul>
+      let newList = document.createElement("ul");
 
+      // Parcourez les membres de l'équipe
+      members.forEach((member) => {
+        // Parcourez les pouvoirs de chaque membre et créez un élément <li> pour chaque pouvoir
+        member.powers.forEach((power) => {
+          let listItem = document.createElement("li");
+          listItem.textContent = `Member: ${member.name}, Power: ${power}`;
+          newList.appendChild(listItem);
+        });
+      });
+
+      // Ajoutez la liste <ul> à la page
+      document.body.appendChild(newList);
+    })
+    .catch((error) => {
+      console.error("Erreur : " + error);
+    });
 });
