7690f6ec635642166e6459e887b3551ece382110
Lucas Beauloi <l.beauloi@gmail.com>
2023-10-09 11:00:18 +0200

    pause 11h , rethink ex vendredi

--- script.js
+++ script.js
@@ -4,6 +4,7 @@
 // créer le bouton en js
 // je sais pas encore comment mais "intégrer" le fichier json au js pour pouvoir prendre les éléments que l'ont veut mettre dans la liste
 // eventlistener sur le bouton pour créer une ul
+
 let title = document.createElement("h1");
 let bouton = document.createElement("button");
 bouton.textContent = "click";
@@ -11,31 +12,37 @@ title.textContent = "Asynchrone exercice 1";
 document.body.appendChild(title);
 document.body.appendChild(bouton);
 
+let fetchSuperHeros = fetch("superheros.json");
 
-bouton.addEventListener("click", function createList() {
+function createList() {
   fetch("superheros.json")
     .then((response) => response.json())
     .then((data) => {
-      // Accédez à la propriété "members" qui contient les membres de l'équipe
+      // via "members" on accède aux heros de la squad de heros
       let members = data.members;
 
-      // Créez la liste <ul>
+      // creation liste ul
       let newList = document.createElement("ul");
 
-      // Parcourez les membres de l'équipe
+      // Pour chaque membres de la hero squad ...
       members.forEach((member) => {
-        // Parcourez les pouvoirs de chaque membre et créez un élément <li> pour chaque pouvoir
+        //...on créé une ul
+        let listeHeros = document.createElement("ul");
+        listeHeros.textContent = `hero: ${member.name}`;
+        document.body.appendChild(listeHeros);
+        // ... on parcoure les pouvoirs de chaque hero et créez un élément li pour chaque pouvoir
         member.powers.forEach((power) => {
           let listItem = document.createElement("li");
-          listItem.textContent = `Member: ${member.name}, Power: ${power}`;
-          newList.appendChild(listItem);
+          listItem.textContent = `Power: ${power}`;
+          listeHeros.appendChild(listItem);
         });
       });
 
-      // Ajoutez la liste <ul> à la page
+      // on rattache la liste créée au body
       document.body.appendChild(newList);
     })
     .catch((error) => {
       console.error("Erreur : " + error);
     });
-});
+}
+bouton.addEventListener("click", createList);
